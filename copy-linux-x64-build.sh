1b70ab2c83a01ddf49bf4f256b0adfac9d8eaf92
Dan Ehrman <accounts.dehrman@gmail.com>
2026-01-02 11:05:08 -0800

    adding scripts to copy build into other repo

---
+++ copy-linux-x64-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ~/code/node-webrtc
+src=./build-linux-x64/Release/wrtc.node
+dst=~/code/main/toeeo-camera-discovery-backend/resources/wrtc/linux-x64/wrtc.node
+tmp="${dst}.tmp.$$"
+rm -f "$tmp"
+cat "$src" > "$tmp"
+chmod 755 "$tmp"
+mv -f "$tmp" "$dst"
+python3 - <<'PY'   
+import pathlib,binascii
+p=pathlib.Path("../main/toeeo-camera-discovery-backend/resources/wrtc/linux-x64/wrtc.node")
+b=p.read_bytes()[:16]
+print("head",binascii.hexlify(b).decode())
+PY
